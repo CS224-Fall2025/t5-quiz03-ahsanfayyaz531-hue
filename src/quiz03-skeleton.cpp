89dbee322979aa43f1f5115411e39b42d94ad48b
Ahsan Fayyaz <af07220@st.habib.edu.pk>
2025-11-14 09:26:51 +0500

    Q4

--- src/quiz03-skeleton.cpp
+++ src/quiz03-skeleton.cpp
@@ -1,5 +1,93 @@
 #include <iostream>
+using namespace std;
+
+
+class Employee{
+    private:
+        static int empcount;
+    protected:
+        string name;
+        double salary;
+    public:
+        Employee(string n, double s): name(n), salary(s){
+            empcount+=1;
+        }
+
+        virtual void displayinfo(){
+            cout << "Name: " << name << endl;
+            cout << "Salary: " << salary << endl;
+        }
+
+        static int showtotalemployee(){
+            return empcount;
+        }
+
+        virtual ~ Employee(){
+            cout << "Destroy Employee" << endl;
+        }
+
+};
+
+int Employee::empcount = 0;
+
+class staff: public Employee{
+    private:
+        string department;
+    public:
+        staff(string n, double s, string d): Employee(n,s), department(d){
+
+
+        }
+        void displayinfo() override{
+            cout << "Name: " << name << endl;
+            cout << " (" << department <<") ";
+            cout << "Salary: " << salary << endl;
+        }
+
+        ~staff(){
+            cout << "destroy staff"<< endl;
+        }
+};
+
+class faculty: public Employee{
+    private:
+        string department;
+    public:
+        faculty(string n, double s, string d): Employee(n,s), department(d){
+
+        }
+
+        void displayinfo() override{
+            cout << "Name: " << name << endl;
+            cout << " (" << department <<") ";
+            cout << "Salary: " << salary << endl;
+        }
+
+        ~faculty(){
+            cout << "destroy faculty"<< endl;
+        }
+};
+
+
+
 
 int main() {
+    int n;
+    cout << "How many numbers of employee? " << endl;
+    cin >> n;
+    Employee* emp[n];
+    //assuming that n = 2;
+    int x = Employee::showtotalemployee();
+    cout << "Total employees: " << x << endl;
+    emp[0] = new staff("Ali", 7500, "staff");
+    emp[1] = new faculty("Salman", 8000, "faculty");
+    for(int i = 0; i < n; i++){
+        emp[i] -> displayinfo();
+    }
+    for(int i = 0; i < n; i++){
+        delete emp[i];
+    }
+
+
 
 }
